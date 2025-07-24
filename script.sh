eedde1e5a49d8bb3819b199bdd09576731dd610f
supanut741355 <supanut.wizard@gmail.com>
2025-07-24 16:28:44 +0700

    fix pv, pvc

--- script.sh
+++ script.sh
@@ -2,5 +2,14 @@ helm upgrade --install nut-grafana-poc grafana/grafana --version 9.2.10 \
   -n grafana-poc \
   -f /Users/nutx/Desktop/Workspaces/PoC/grafana-pv-mount-dashboard/values.yml 
 
-
+#ns
 kubectl create ns grafana-poc
+
+
+#pv
+kubectl apply -f /Users/nutx/Desktop/Workspaces/PoC/grafana-pv-mount-dashboard/pv.yml -n grafana-poc
+
+#pvc
+kubectl apply -f /Users/nutx/Desktop/Workspaces/PoC/grafana-pv-mount-dashboard/pvc.yml -n grafana-poc
+
+:pvc
