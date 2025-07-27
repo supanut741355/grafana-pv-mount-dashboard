590fda2370b87a99f565a01639654bb459c59e9b
supanut741355 <supanut.wizard@gmail.com>
2025-07-27 21:57:31 +0700

    add value

--- script.sh
+++ script.sh
@@ -2,6 +2,10 @@ helm upgrade --install nut-grafana-poc grafana/grafana --version 9.2.10 \
   -n grafana-poc \
   -f /Users/nutx/Desktop/Workspaces/PoC/grafana-pv-mount-dashboard/values.yml 
 
+helm uninstall nut-grafana-poc grafana/grafana -n grafana-poc 
+
+
+
 #ns
 kubectl create ns grafana-poc
 
